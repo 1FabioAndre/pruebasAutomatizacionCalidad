2bb192d6a99ca333c518f6d75b8143c9f15107bb
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2024-12-01 17:48:18 -0400

    Solucionanado error

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,6 +2,10 @@
 After do 
     Capybara.current_session.driver.quit
 end
+After do
+    Capybara.reset_sessions!
+    page.driver.browser.manage.delete_all_cookies
+  end
 
 Before '@maximize' do
     page.driver.browser.manage.window.maximize
