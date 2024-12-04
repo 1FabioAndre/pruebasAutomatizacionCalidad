7d795353def70461214638ccbab59bc33776a6e4
FabioMojica <fabioandremojicaarmaza@gmail.com>
2024-12-04 14:28:50 -0400

    Descripcion en los steps HomePage

--- features/step_definitions/HomePage_steps.rb
+++ features/step_definitions/HomePage_steps.rb
@@ -1,10 +1,14 @@
+# Scenario: Verify homepage elements
 Given('I am on the homepage to GreenMountain') do
   visit 'https://demo.borland.com/gmopost/'
 end
 
+# Step to check the homepage elements
 When('I look at the homepage') do
+  # Aquí puedes agregar una verificación si lo deseas
 end
 
+# Verificación de botones en la página de inicio
 And('I should see the "Enter GMO OnLine" button on the homepage') do
   expect(page).to have_button('Enter GMO OnLine', wait: 10)
 end
@@ -17,19 +21,23 @@ And('I should see the "Browser Test Page" button on the homepage') do
   expect(page).to have_button('Browser Test Page', wait: 10)
 end
 
+# Verificación de la imagen del logo
 And('I should see the logo on the homepage') do
   expect(page).to have_selector('img[src="images/gmo-animate.gif"]', visible: true)
   expect(find('img[src="images/gmo-animate.gif"]')[:alt]).to eq('Logo')
 end
 
+# Verificación de enlaces
 And('I should see a link to the "mailto:gmo-master@segue.com" email address on the homepage') do
   expect(page).to have_link('Please let us know how you like our site.', href: 'mailto:gmo-master@segue.com')
 end
 
+# Verificación de títulos
 And('I should see the "GMO OnLine" title on the homepage') do
   expect(page).to have_selector('h1', text: 'GMO OnLine', wait: 10)
 end
 
+# Verificación de texto de bienvenida
 And('I should see the "Welcome to Green Mountain Outpost\'s OnLine Catalog!" text on the homepage') do
   expect(page).to have_content("Welcome to Green Mountain Outpost's OnLine Catalog!", wait: 10)
 end
